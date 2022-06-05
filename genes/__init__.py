95c94362b753fe62a2a84ae6839d7c73c88dfb4d
Łukasz Bacik <mail@luka.sh>
2022-06-05 10:50:30 +0200

    Update package init file

--- genes/__init__.py
+++ genes/__init__.py
@@ -1,15 +1,15 @@
 """
 Genes
+
+Simple library for playing with genetic algorithms
+
+Documentation: https://github.com/lbacik/genes
+
 """
 
-from .genotype.phenotype import Phenotype
+from .genotype.genotype import Genotype
 from .genotype.population import Population
-from .genotype.operatord.crossover import Crossover
-from .genotype.operatord.mutation import Mutation
-from .genotype.operatord.selection import Selection
-from .genalg.operator_list import OperatorList
 from .genotype.fitness_function import FitnessFunction
 from .genotype.populationd import factory as population_factory
-# from .genalg.factory import create_from_operator_list as genes_api_from_operator_list_factory
 from .genalg.factoryd.params import GAFactoryParams
 from .genalg.factory import create_from_operator_params as genes_api_from_params_factory
